3cc54b1f202e357b20b59b3da3d601975e2ac87a
Parveshbhatt <parveshbhatt118@gmail.com>
2023-03-22 12:16:25 +0530

    refactoring of code

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,22 +9,10 @@ export class ProductsService{
 
   products = [];
   isLoading = new BehaviorSubject<boolean>(true);
-  
-
-  // raiseLoadEvent(loading:boolean){
-  //   this.isLoading.next(loading);
-  // }
 
   constructor(private http: HttpClient) {
     console.log("auth constructor");
-    
-    // console.log(this.isLoading);
-    // this.raiseLoadEvent(true);
     this.isLoading.next(true);
-    // console.log("in product service laod:");
-    // this.isLoading.subscribe((value) => {
-    //   console.log("lB: "+value)
-    // });
     this.http.get('https://dummyjson.com/products').subscribe((res) => {
       console.log(res['products']);
       for(const key in res['products']){
@@ -32,59 +20,14 @@ export class ProductsService{
           this.products.push({...res['products'][key]});
         }
       }
-      // for(const key in res){
-      //   if(res.hasOwnProperty(key)){
-      //     this.products.push({...res[key]});
-      //   }
-      // }
-      // this.setIsLoading(false);
-      // this.raiseLoadEvent(false);
       this.isLoading.next(false);
       console.log(this.products);
-      // console.log("in product service laod after:");
-      // this.isLoading.subscribe((value) => {
-      //   console.log("lA: "+value)
-      // });
       
     });
-    // this.isLoading = false;
     
    }
 
    fetchProducts(){
     return false;
    }
-
-
-  //  ngOnInit(){
-    // this.fetchProducts();
-    // this.http.get('https://fakestoreapi.com/products').subscribe((res) => {
-    //   for(const key in res){
-    //     if(res.hasOwnProperty(key)){
-    //       this.products.push({...res[key]});
-    //     }
-    //   }
-    //   console.log(this.products);
-    // });
-    // console.log("product service on init ");
-    // this.isLoading = false;
-  //  }
-
-  //  ngAfterContentChecked(): void {
-  //      this.isLoading = false;
-  //  }
-
-  // fetchProducts(){
-    
-  // }
-
-  //getter , setter
-
-  // setIsLoading(value:boolean){
-  //   this.isLoading = value;
-  // }
-
-  // getIsLoading():boolean{
-  //   return this.isLoading;
-  // }
 }
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -7,44 +7,14 @@ import { AppState } from '../app.state';
   providedIn: 'root'
 })
 export class ThemeService{
-  // themeSubscribtion: Subscription;
-  // storedTheme:string;
-  // themeEmitter = new EventEmitter<string>();
 
   
   themeEmitter = new BehaviorSubject<string>(localStorage.getItem('theme-color'));
   
   constructor(private store: Store<AppState>) { 
-    // this.themeSubscribtion = this.store.select('theme').subscribe((data) => {
-    //   this.setTheme(data.theme);
-    //   console.log("Theme Service: "+ this.getTheme());
-    // });
   }
 
   raiseThemeEvent(theme:string){
     this.themeEmitter.next(theme);
   }
-
-  // ngDoCheck(){
-  //   // this.themeSubscribtion = this.store.select('theme').subscribe((data) => {
-  //   //   this.setTheme(data.theme);
-  //   //   console.log("Theme Service: "+ this.getTheme());
-  //   // });
-  // }
-
-  // ngOnDestroy(){
-  //   // if(this.themeSubscribtion){
-  //   //   this.themeSubscribtion.unsubscribe();
-  //   // }
-  // }
-
-  // setTheme(theme:string){
-  //   this.storedTheme = theme;
-  // }
-
-  // getTheme(){
-  //   return this.storedTheme;
-  // }
-
- 
 }
